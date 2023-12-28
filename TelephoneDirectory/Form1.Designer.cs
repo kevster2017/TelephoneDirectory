b60715f1dd0e716c0513b22206a603cf27666695
kevster2017 <kevin.okane2017@gmail.com>
2023-12-28 21:15:05 +0000

    Database Connection added

--- TelephoneDirectory/Form1.Designer.cs
+++ TelephoneDirectory/Form1.Designer.cs
@@ -28,96 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
+            btnNew = new Button();
+            btnInsert = new Button();
+            txtFirstName = new TextBox();
+            txtSurname = new TextBox();
+            txtMobile = new TextBox();
+            txtEmail = new TextBox();
             dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
             comboBox1 = new ComboBox();
+            btnDelete = new Button();
+            btnUpdate = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // btnNew
             // 
-            button1.Location = new Point(756, 362);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnNew.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNew.Location = new Point(597, 60);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(137, 53);
+            btnNew.TabIndex = 5;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = true;
+            btnNew.Click += btnNew_Click;
             // 
-            // button2
+            // btnInsert
             // 
-            button2.Location = new Point(809, 303);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnInsert.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnInsert.Location = new Point(597, 113);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(137, 53);
+            btnInsert.TabIndex = 6;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
             // 
-            // textBox1
+            // txtFirstName
             // 
-            textBox1.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(211, 123);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(281, 31);
-            textBox1.TabIndex = 2;
+            txtFirstName.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtFirstName.Location = new Point(207, 60);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(281, 31);
+            txtFirstName.TabIndex = 0;
             // 
-            // textBox2
+            // txtSurname
             // 
-            textBox2.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(211, 165);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(281, 31);
-            textBox2.TabIndex = 3;
+            txtSurname.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSurname.Location = new Point(207, 102);
+            txtSurname.Name = "txtSurname";
+            txtSurname.Size = new Size(281, 31);
+            txtSurname.TabIndex = 1;
             // 
-            // textBox3
+            // txtMobile
             // 
-            textBox3.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox3.Location = new Point(211, 207);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(281, 31);
-            textBox3.TabIndex = 4;
+            txtMobile.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMobile.Location = new Point(207, 144);
+            txtMobile.Name = "txtMobile";
+            txtMobile.Size = new Size(281, 31);
+            txtMobile.TabIndex = 2;
             // 
-            // textBox4
+            // txtEmail
             // 
-            textBox4.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox4.Location = new Point(211, 249);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(281, 31);
-            textBox4.TabIndex = 5;
+            txtEmail.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtEmail.Location = new Point(207, 186);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(281, 31);
+            txtEmail.TabIndex = 3;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 414);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            dataGridView1.Location = new Point(12, 338);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(1215, 179);
-            dataGridView1.TabIndex = 7;
+            dataGridView1.Size = new Size(1215, 271);
+            dataGridView1.TabIndex = 12;
+            // 
+            // Column1
+            // 
+            Column1.FillWeight = 150F;
+            Column1.HeaderText = "First Name";
+            Column1.Name = "Column1";
+            Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            Column2.FillWeight = 150F;
+            Column2.HeaderText = "Surname";
+            Column2.Name = "Column2";
+            Column2.Width = 200;
+            // 
+            // Column3
+            // 
+            Column3.FillWeight = 150F;
+            Column3.HeaderText = "Mobile";
+            Column3.Name = "Column3";
+            Column3.Width = 200;
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column4.FillWeight = 150F;
+            Column4.HeaderText = "Email";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.FillWeight = 150F;
+            Column5.HeaderText = "Category";
+            Column5.Name = "Column5";
+            Column5.Width = 200;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(80, 123);
+            label1.Location = new Point(76, 60);
             label1.Name = "label1";
             label1.Size = new Size(111, 25);
-            label1.TabIndex = 8;
+            label1.TabIndex = 7;
             label1.Text = "First Name :";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(80, 207);
+            label2.Location = new Point(76, 144);
             label2.Name = "label2";
             label2.Size = new Size(85, 25);
             label2.TabIndex = 9;
@@ -127,7 +174,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(80, 249);
+            label3.Location = new Point(76, 186);
             label3.Name = "label3";
             label3.Size = new Size(67, 25);
             label3.TabIndex = 10;
@@ -137,7 +184,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(80, 291);
+            label4.Location = new Point(76, 228);
             label4.Name = "label4";
             label4.Size = new Size(97, 25);
             label4.TabIndex = 11;
@@ -147,21 +194,42 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(80, 165);
+            label5.Location = new Point(76, 102);
             label5.Name = "label5";
             label5.Size = new Size(96, 25);
-            label5.TabIndex = 12;
+            label5.TabIndex = 8;
             label5.Text = "Surname :";
             // 
             // comboBox1
             // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "HOME", "OFFICE", "BUSINESS", "FRIENDS", "FAMILY" });
-            comboBox1.Location = new Point(211, 291);
+            comboBox1.Location = new Point(207, 228);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(281, 33);
-            comboBox1.TabIndex = 13;
+            comboBox1.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDelete.Location = new Point(597, 166);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(137, 53);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnUpdate.Location = new Point(597, 219);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(137, 53);
+            btnUpdate.TabIndex = 6;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -175,12 +243,14 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(txtEmail);
+            Controls.Add(txtMobile);
+            Controls.Add(txtSurname);
+            Controls.Add(txtFirstName);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
+            Controls.Add(btnInsert);
+            Controls.Add(btnNew);
             Name = "Form1";
             Text = "Telephone Directory";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -190,12 +260,12 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
+        private Button btnNew;
+        private Button btnInsert;
+        private TextBox txtFirstName;
+        private TextBox txtSurname;
+        private TextBox txtMobile;
+        private TextBox txtEmail;
         private DataGridView dataGridView1;
         private Label label1;
         private Label label2;
@@ -203,5 +273,12 @@
         private Label label4;
         private Label label5;
         private ComboBox comboBox1;
+        private Button btnDelete;
+        private Button btnUpdate;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
     }
 }
--- TelephoneDirectory/Form1.cs
+++ TelephoneDirectory/Form1.cs
@@ -1,10 +1,59 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
 namespace TelephoneDirectory
 {
+
+ 
     public partial class Form1 : Form
     {
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Phone;Integrated Security=True"); // Connection string as the parameter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Clear();
+            txtSurname.Clear();
+            txtMobile.Clear();
+            txtEmail.Clear();
+            comboBox1.SelectedIndex = -1;  // -1 used to clear combo box ( 0 = First Option )
+            txtFirstName.Focus(); // Focuses cursor on first name field when data is cleared
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO Mobiles " +
+                    "(FirstName, Surname, Mobile, Email, Category) " +
+                    "VALUES (@FirstName, @Surname, @Mobile, @Email, @Category)", con);
+
+                // Use parameters to prevent SQL injection
+                cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                cmd.Parameters.AddWithValue("@Surname", txtSurname.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@Category", comboBox1.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
